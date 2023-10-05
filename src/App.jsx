5e8b89f9fe7af3682e39c6f994665af70a5341a4
ARN1380 <alireza.naghavi0@gmail.com>
2023-10-05 11:34:56 +0330

    added figam link to the app page in comments for futher changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Dashboard from './components/pages/user/Dashboard';
 import MainLayout from './Layouts/MainLayout';
 
 
-
+// https://www.figma.com/file/evJyhfeRqgwROLy34dwnai/FinanceFlow%3A-Website-Template-for-Finance-startups-(Community)?type=design&node-id=1-3&mode=design&t=25duKGPWA3X8kuQY-0
 
 
 function App() {
